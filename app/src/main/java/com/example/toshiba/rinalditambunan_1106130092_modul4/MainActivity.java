e2b0bc3c801eb155450087b89163d7647ae17e23
rinaldi163 <rinaldi.aldi47@yahoo.com>
2018-03-18 23:29:24 +0700

    push pertama kali

---
+++ app/src/main/java/com/example/toshiba/rinalditambunan_1106130092_modul4/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.toshiba.rinalditambunan_1106130092_modul4;
+
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void listNama(View view) {
+        Intent a = new Intent(MainActivity.this,ListNama.class);
+        startActivity(a);
+    }
+
+    public void cariGambar(View view) {
+        Intent b = new Intent(MainActivity.this,CariGambar.class);
+        startActivity(b);
+    }
+}
+
